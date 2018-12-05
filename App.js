68efd967c342ffc7477835e404dfcfbbcf958058
Sungmiii <christyksmile@gmail.com>
2018-12-06 11:34:15 +1300

    map

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-import BoorklynMap from './src/components'
+import BoorklynMap from './src/components/BoorklynMap'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Yoo!s</Text>
-        <BoorklynMap />
-      </View>
+      <BoorklynMap />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
