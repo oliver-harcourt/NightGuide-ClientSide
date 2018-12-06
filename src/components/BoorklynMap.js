e8c45b1ba8862eee4027c510a05567c001894218
Sungmiii <christyksmile@gmail.com>
2018-12-06 16:28:27 +1300

    aim to get last version

--- src/components/BoorklynMap.js
+++ src/components/BoorklynMap.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { MapView } from 'expo'
 
+const mapList = [
+    { id: 1, name: 'Wellington Wind Turbine', latitude: -41.310868, longitude: 174.744861 },
+    { id: 2, name: 'Mount Victoria Lookout', latitude: -41.295932, longitude: 174.794568 },
+    {
+        id: 3, name: 'Te Ahumairangi Hill Lookout', latitude: -41.274925, longitude: 174.765356
+    },
+    { id: 4, name: 'Red Rocks', latitude: -41.357376, longitude: 174.726562 }
+
+]
+
 export default class BooklynMap extends React.Component {
     render() {
         return (
