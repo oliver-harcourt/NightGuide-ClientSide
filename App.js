be41f82f2bbcc03cda064ba8f7d72fbcc87436f9
tkurudi <t.kurudi209@gmail.com>
2018-12-07 09:28:42 +1300

    added temp, city, country, description to weather coponent and cleaned up app.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View } from "react-native";
 
 import BrooklynMap from "./src/components/BrooklynMap";
 import ConstellationDetails from "./src/components/ConstellationDetails";
@@ -13,73 +14,25 @@ import PlanetDetails from "./src/components/PlanetDetails";
 import PlanetList from "./src/components/PlanetList";
 import Weather from "./src/components/Weather";
 import WelcomeInstruction from "./src/components/WelcomeInstruction";
-import { StyleSheet, Text, View, ActivityIndicator} from 'react-native'
- 
 
 export default class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isLoading: true,
-      dataSource: null,
-
-    }
-  }
-  componentDidMount() {
-    return fetch('http://api.openweathermap.org/data/2.5/weather?q=Wellington,NZ&appid=6077bc0c24cd3abfdf63f56070c1d010&units=metric')
-  .then((response) => response.json())
-  .then((responseJson) => {
-    this.setState({
-    isLoading: false,
-    dataSource: responseJson.weather,
-
-  })
-  })
-  .catch((error) =>{
-    console.log(error)
-  })
-  }
-
   render() {
-    if (this.state.isLoading){
-      return (
-        <View styles={StyleSheet.container}>
-        <ActivityIndicator />
-        </View>
-      )
-    } else {
-
-      let weather = this.state.dataSource.map((val, key) => {
-        return <View key={key}>
-        <Text>{val.description}</Text>
-        </View>
-      })
-    
     return (
       <View>
-        {/* <BrooklynMap />
-        <ConstellationDetails />
+        {/* <BrooklynMap /> */}
+        {/* <ConstellationDetails />
         <ConstellationList /> */}
-        <Header />
+        {/* <Header />
         <InstructionContainer />
         <Landing />
         <MenuInstruction />
         <NavBar />
         <NightGuideLogo />
         <PlanetDetails />
-        <PlanetList />
-        <WelcomeInstruction />
-        {weather}
+        <PlanetList /> */}
+        <Weather />
+        {/* <WelcomeInstruction /> */}
       </View>
     );
   }
 }
-}
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
-  },
-});
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,61 +1,59 @@
-import t from 'tcomb-form-native';
 import React from "react";
-import { Text, View } from "react-native";
-const API_KEY = '6077bc0c24cd3abfdf63f56070c1d010'
+// import NavBar from "./src/components/NavBar";
+import { StyleSheet, Text, View, ActivityIndicator} from 'react-native'
+ 
 
-const form = t.struct({
-  city: t.String,
-  country: t.String
-})
-export default class Weather extends React.Component {
-  state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
-  }
-
-  getWeather = async (e) => {
-    e.preventDefault();
-    const city = e.target.elements.city.value
-    const country = e.target.elements.country.value
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    if (city && country) {
-      console.log(data);
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      })
-    } else {
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "Please Enter A Value"
-
-      })
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoading: true,
+      dataSource: null,
 
     }
   }
+  componentDidMount() {
+    return fetch('http://api.openweathermap.org/data/2.5/weather?q=Wellington,NZ&appid=6077bc0c24cd3abfdf63f56070c1d010&units=metric')
+  .then((response) => response.json())
+  .then((responseJson) => {
+    this.setState({
+    isLoading: false,
+    dataSource: responseJson
+
+  })
+  })
+  .catch((error) =>{
+    console.log(error)
+  })
+  }
+
   render() {
+    if (this.state.isLoading){
+      return (
+        <View styles={StyleSheet.container}>
+        <ActivityIndicator />
+        </View>
+      )
+    } else {
+
+      let data = this.state.dataSource
+    
     return (
-      <View>
-        <Text>{form}</Text>
-        <Text>{this.state.temperature}</Text>
-        <Text>{this.state.city}</Text>
-        <Text>{this.state.country}</Text>
-        <Text>{this.state.humidity}</Text>
-        <Text>{this.state.description}</Text>
+      <View style={styles}>
+       <Text> Temprature: {data.main.temp}</Text>
+       <Text> City: {data.name}</Text>
+       <Text> Country: {data.sys.country}</Text>
+       <Text> Description: {data.weather[0].description}</Text>
       </View>
     );
   }
 }
+}
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+});
