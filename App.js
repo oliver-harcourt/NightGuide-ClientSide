cfb11354f1596c8df5ab79acf58ffb10d227b5df
Sungmiii <christyksmile@gmail.com>
2018-12-07 11:00:55 +1300

    merge confilict check

--- App.js
+++ App.js
@@ -2,20 +2,10 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import BrooklynMap from "./src/components/BrooklynMap";
-import ConstellationDetails from "./src/components/ConstellationDetails";
-import ConstellationList from "./src/components/ConstellationList";
-import Header from "./src/components/Header";
-import InstructionContainer from "./src/components/InstructionContainer";
 import Landing from "./src/components/Landing";
 import Home from './src/components/Home'
-import MenuInstruction from "./src/components/MenuInstruction";
-import NavBar from "./src/components/Navbar/NavBar";
-import NightGuideLogo from "./src/components/NightGuideLogo";
-import PlanetDetails from "./src/components/PlanetDetails";
-import PlanetList from "./src/components/PlanetList";
 import Weather from "./src/components/Weather";
-import WelcomeInstruction from "./src/components/WelcomeInstruction";
+
 
 const RootStack = createStackNavigator({
   Landing: {
