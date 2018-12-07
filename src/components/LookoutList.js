4d9a1580c616fbaa21f9b2dbd032bb49eedc71a7
Kelly <kellyandstone@gmail.com>
2018-12-07 13:40:38 +1300

    lookoutList done

--- src/components/LookoutList.js
+++ src/components/LookoutList.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import lookouts from "../../data/lookouts";
 
+import { View, Text } from "react-native";
 
-class LookoutList extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>LookOut</Text>
-            </View>
-        )
-    }
+import { List, ListItem } from "react-native-elements";
+
+export default class LookoutDetails extends React.Component {
+  render() {
+    return (
+      <View>
+        <List containerStyle={{ marginBottom: 20 }}>
+          {lookouts.map(lookout => (
+            <ListItem
+              roundAvatar
+              avatar={{ uri: lookout.icon }}
+              key={lookout.id}
+              title={lookout.name}
+            />
+          ))}
+        </List>
+      </View>
+    );
+  }
 }
-
-export default LookoutList
