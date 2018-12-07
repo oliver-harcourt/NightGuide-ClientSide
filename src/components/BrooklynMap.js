b808bce3acecff961b185b616c1c80a9630b98a3
Kelly <kellyandstone@gmail.com>
2018-12-07 16:00:43 +1300

    edit red rock latitude

--- src/components/BrooklynMap.js
+++ src/components/BrooklynMap.js
@@ -17,7 +17,24 @@ export default class BrooklynMap extends React.Component {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }}
-      />
+      >
+        <MapView.Marker
+          coordinate={{ latitude: -41.295932, longitude: 174.794568 }}
+          title={"Mount Victoria Lookout"}
+        />
+        <MapView.Marker
+          coordinate={{ latitude: -41.274925, longitude: 174.765356 }}
+          title={"Te Ahumairangi Hill Lookout"}
+        />
+        <MapView.Marker
+          coordinate={{ latitude: -41.310868, longitude: 174.744861 }}
+          title={"Wellington Wind Turbine"}
+        />
+        <MapView.Marker
+          coordinate={{ latitude: -41.3408, longitude: 174.7591 }}
+          title={"Red Rocks"}
+        />
+      </MapView>
     );
   }
 }
