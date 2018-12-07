74efe5e6399399c929487b325e8b983d36189637
oliver-harcourt <oliver.harcourt@gmail.com>
2018-12-07 13:44:43 +1300

    planetlist working

--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import planets from '../../data/planets'
+
+import {
+  View,
+  Text
+} from 'react-native'
+
+import {
+  List,
+  ListItem
+} from 'react-native-elements'
 
 export default class PlanetList extends React.Component {
   render() {
     return (
       <View>
-        <Text>PlanetList</Text>
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            planets.map((planet) => (
+              <ListItem
+                roundAvatar
+                avatar={{uri:planet.image}}
+                key={planet.name}
+                title={planet.name}
+                // leftIcon={{ name: planet.image }}
+              />
+            ))
+          }
+        </List>
       </View>
-    );
+    )
   }
 }
+
