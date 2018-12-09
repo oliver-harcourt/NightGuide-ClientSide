3bf545a835413dd8750c6912061849e9a1353f6d
tkurudi <t.kurudi209@gmail.com>
2018-12-10 12:03:08 +1300

    time check for sungmi

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,45 +1,68 @@
 import React from "react";
-import { StyleSheet, Text, View, ActivityIndicator, ImageBackground } from 'react-native'
-
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground
+} from "react-native";
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoading: true,
       dataSource: null,
-    }
+      sunRise: false,
+      sunSet: false
+    };
+    
+    this.isNearlySunset = this.isNearlySunset.bind(this)
+     
   }
   componentDidMount() {
-    return fetch('http://api.openweathermap.org/data/2.5/weather?q=Wellington,NZ&appid=6077bc0c24cd3abfdf63f56070c1d010&units=metric')
-      .then((response) => response.json())
-      .then((responseJson) => {
+    return fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=Wellington,NZ&appid=6077bc0c24cd3abfdf63f56070c1d010&units=metric"
+    )
+      .then(response => response.json())
+      .then(responseJson => {
         this.setState({
           isLoading: false,
           dataSource: responseJson
-        })
+        });
       })
-      .catch((error) => {
-        console.log(error)
-      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
+  isNearlySunset(sunsetTime) {
+    let currentTime = new Date().getTime() / 1000;
+    return currentTime > sunsetTime - 60 * 60 * 2;
+  }
+   
   render() {
+    const data = this.state.dataSource;
+    console.log(data);
+
+    
     if (this.state.isLoading) {
       return (
         <View styles={StyleSheet.container}>
           <ActivityIndicator />
         </View>
-      )
+      );
     } else {
-
-      let data = this.state.dataSource
-
       return (
+        
+       
         <ImageBackground
-          source={require('../../assets/bgeffect.gif')}
-          style={StyleSheet.absoluteFill}>
+          source={(this.isNearlySunset(data.sys.sunset)) ?
+            require("../../assets/sunny.gif") : require("../../assets/bgeffect.gif") }
+
+
+          style={StyleSheet.absoluteFill}
+        >
           <View style={styles.container}>
             <Text style={styles.title}> {data.name}</Text>
             <Text style={styles.subtitle}>{data.sys.country}</Text>
@@ -47,49 +70,51 @@ export default class App extends React.Component {
             <Text style={styles.descrip}>{data.weather[0].description}</Text>
           </View>
         </ImageBackground>
+          
       );
     }
   }
 }
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     marginTop: 100,
-    padding: 20,
+    padding: 20
   },
   bgStyle: {
-    resizeMode: 'stretch',
+    resizeMode: "stretch"
   },
   title: {
     fontSize: 50,
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     left: 10,
-    fontWeight: 'bold',
-    color: 'white'
+    fontWeight: "bold",
+    color: "white"
   },
   subtitle: {
     fontSize: 20,
-    position: 'absolute',
+    position: "absolute",
     top: 110,
     left: 235,
-    color: 'white',
-    fontWeight: '700'
+    color: "white",
+    fontWeight: "700"
   },
   tempstyle: {
     fontSize: 80,
-    color: 'white',
-    position: 'absolute',
+    color: "white",
+    position: "absolute",
     top: 300,
     left: 20,
-    fontWeight: '700'
+    fontWeight: "700"
   },
   descrip: {
     fontSize: 20,
-    color: 'white',
-    fontWeight: '700',
-    position: 'absolute',
+    color: "white",
+    fontWeight: "700",
+    position: "absolute",
     top: 350,
-    left: 150,
+    left: 150
   }
 });
+//this.isNearlySunset && require("../../assets/bgeffect.gif")
