b3c57e5a363ae8d3831264fcf1cff8d5ae33f417
Sungmiii <christyksmile@gmail.com>
2018-12-10 21:10:02 +1300

    starmap home change marginbottom fit to iphone xr

--- src/components/StarMap.js
+++ src/components/StarMap.js
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
         alignItems: 'stretch'
     },
     webView: {
-        marginBottom: -385,
+        marginBottom: -500,
         backgroundColor: 'black'
     }
 });
