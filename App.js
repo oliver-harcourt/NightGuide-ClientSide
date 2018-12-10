3de89f116d0dba3249d299236426181e96465ddd
Joan-SHI <qiongnashi@gmail.com>
2018-12-11 09:20:30 +1300

    fix initial scene back to login component

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default class App extends React.Component {
           <Scene
             key="login"
             component={Login}
-
+            initial
             hideNavBar={true}
           />
           <Scene
@@ -30,7 +30,6 @@ export default class App extends React.Component {
           />
           <Scene
             key="home"
-            initial
             component={Home}
             title="Home"
           />
