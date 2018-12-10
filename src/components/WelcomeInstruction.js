86cf1fbf4ff0ddc7f03e94ce4ad13decb999d86b
Joan-SHI <qiongnashi@gmail.com>
2018-12-10 15:51:39 +1300

    decorate button on swipe4

--- src/components/WelcomeInstruction.js
+++ src/components/WelcomeInstruction.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Swiper from 'react-native-swiper'
 import { Actions } from 'react-native-router-flux'
-import { ImageBackground, View, StyleSheet, Text, Button, Image, Dimensions, ScrollView } from "react-native";
+import { ImageBackground, View, StyleSheet, Text, Button, Image, Dimensions, ScrollView, TouchableOpacity } from "react-native";
 //mport instructions from '../../data/instructions'
 //import {List,ListItem} from 'react-native-elements'
 // import Lookout from '../components/Navbar/Lookout'
@@ -27,50 +27,62 @@ var styles = {
     justifyContent: 'center',
     alignItems: 'center',
   },
-  
+
   slide4: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#6c5ce7'
+    backgroundColor: '#6c5ce7' 
   },
   text: {
     color: '#fff',
     fontSize: 27,
     fontWeight: 'bold'
-  }
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#f3a683',
+    padding: 10,
+  },
 }
 
-export default () => 
-<Swiper style={styles.wrapper} 
-showsPagination={false}
->
-  <View style={styles.slide1}>
-        <Image
-          resizeMode="contain"
-          source={require('../../assets/instruction/instructionOne.gif')}
-          style={{width:"100%"}}
-          />
-      </View>
-<View style={styles.slide2} >
-          <Image
-          resizeMode="contain"
-          source={require('../../assets/instruction/instructionTwo.gif')}
-          style={{width:"100%"}}
-          />
-  </View>
-  <View style={styles.slide3}>
-  <Image
-          resizeMode="contain"
-          source={require('../../assets/instruction/instructionThree.gif')}
-          style={{width:"100%"}}
-          />
-    
-  </View>
-  <View style={styles.slide4}>
-  <Button onPress={() => Actions.home()} title="Home" color="#841584"></Button>
-  </View>
-</Swiper>
+export default () =>
+  <Swiper style={styles.wrapper}
+    showsPagination={false}
+  >
+    <View style={styles.slide1}>
+      <Image
+        resizeMode="contain"
+        source={require('../../assets/instruction/instructionOne.gif')}
+        style={{ width: "100%" }}
+      />
+    </View>
+    <View style={styles.slide2} >
+      <Image
+        resizeMode="contain"
+        source={require('../../assets/instruction/instructionTwo.gif')}
+        style={{ width: "100%" }}
+      />
+    </View>
+    <View style={styles.slide3}>
+      <Image
+        resizeMode="contain"
+        source={require('../../assets/instruction/instructionThree.gif')}
+        style={{ width: "100%" }}
+      />
+
+    </View>
+    <View style={styles.slide4}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={this.onPress}
+      >
+        <Button onPress={() => Actions.home()} title="Home" color="white"></Button>
+
+      </TouchableOpacity>
+
+    </View>
+  </Swiper>
 
 
 
