7227f0efff7a8a9755227e5ad056a7508de40d90
Sungmiii <christyksmile@gmail.com>
2018-12-12 14:49:54 +1300

    fix mapview

--- src/components/Map.js
+++ src/components/Map.js
@@ -53,7 +53,6 @@ export default class Map extends React.Component {
         showsMyLocationButton={true}
         showsUserLocation={true}
         followsUserLocation={true}
-        showsCompass={true}
         initialRegion={{
           latitude: -41.310868,
           longitude: 174.744861,
